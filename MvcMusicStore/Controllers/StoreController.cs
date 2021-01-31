4514b0a2818369e4899b09af0ddd7bdee3c65537
li <lideng928@gmail.com>
2021-01-30 23:29:51 -0500

    Updated StoreController and created ViewModel and added a shared view for guest

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcMusicStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,26 @@ namespace MvcMusicStore.Controllers
             ViewBag.message = "this is the ViewBag property message";
             return View("Sample");
             //return view() - this will go to a view that has the same action name. 
+        }
+
+        //The List() action with create a new album list and iterate 
+        //the list and output the list in a HTML page
+        public IActionResult List()
+        {
+            List<Album> albums = new List<Album>();
+            for(int i = 0; i < 10; i++)
+            {
+                albums.Add(new Album { Title = "Generic Album " + i });
+            }
+            //ViewBag.Albums = albums; //create the albums list in ViewBag
+            //return View(); //render the view with the name 'List'
+            return View(albums); //returns the albums list model
+        }
+
+        //Create a guest view
+        public IActionResult Guest()
+        {
+            return View();
         }
     }
 }
---
+++ MvcMusicStore/Models/ViewModel/ShoppingCartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Keep it in the same namespace
+namespace MvcMusicStore.Models
+{
+    public class ShoppingCartViewModel
+    {
+        public IEnumerable<Album> albums { get; set; } //This is the albums list we created
+        public string userName { get; set; }
+        public string authenticationLevel { get; set; }
+        public double cartTotal { get; set; }
+
+    }
+}
